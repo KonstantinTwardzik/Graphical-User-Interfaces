ba50546288fe60d0374cd81d4486909ad62d3380
Konstantin <twardzk@hochschule-trier.de>
2017-11-18 10:22:29 +0100

    Exercise6 first push

---
+++ Exercise06/src/gui/country/combo/CountryInfo.java
@@ -0,0 +1,21 @@
+package gui.country.combo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CountryInfo extends Application
+{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+}
